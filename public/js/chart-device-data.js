8b90637d480b96e1fe67d431af8e64260b9c9fdc
Helena S <helenays@uci.edu>
2025-12-03 02:37:10 -0800

    CS147 final project fall detection and alert system modification

--- public/js/chart-device-data.js
+++ public/js/chart-device-data.js
@@ -6,111 +6,166 @@ $(document).ready(() => {
   const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
   const webSocket = new WebSocket(protocol + location.host);
 
-  // A class for holding the last N points of telemetry for a device
+  // A class for holding the last N points of telemetry for the mointored device
   class DeviceData {
-    constructor(deviceId) {
-      this.deviceId = deviceId;
-      this.maxLen = 50;
+    constructor() {
+      this.maxLen = 30;
       this.timeData = new Array(this.maxLen);
-      this.temperatureData = new Array(this.maxLen);
-      this.humidityData = new Array(this.maxLen);
+      this.accData = new Array(this.maxLen);
+      this.gyroData = new Array(this.maxLen);
+      this.state = new Array(this.maxLen);
+      this.stateColor = new Array(this.maxLen);
+      this.detectedCount = 0;
+      this.stillDetected = false;
+      this.alertCount = 0;
+      this.stillAlert = false;
     }
 
-    addData(time, temperature, humidity) {
+    addData(time, acc, gyro, state) {
       this.timeData.push(time);
-      this.temperatureData.push(temperature);
-      this.humidityData.push(humidity || null);
+      this.accData.push(acc);
+      this.gyroData.push(gyro);
+      switch (state) {
+        case "detected":
+          this.state.push(4); // orange
+          this.stateColor.push('rgba(255, 165, 0, 0.4)');
+          if (!this.stillDetected) {
+            this.detectedCount += 1;
+            document.getElementById('detectedCount').innerText = this.detectedCount;
+            this.stillDetected = true;
+          }
+          break;
+        case "alert":
+          this.state.push(8); // red
+          this.stateColor.push('rgba(255, 0, 0, 0.4)');
+          if (!this.stillAlert) {
+            this.stillDetected = false;
+            this.alertCount += 1;
+            // no need to check for zero division because detect always goes first
+            document.getElementById('alertCount').innerText = this.alertCount;
+            document.getElementById('alertRate').innerText = (this.alertCount / this.detectedCount).toFixed(2);
+            this.stillAlert = true;
+          }
+          break;
+        case "canceled":
+          this.state.push(2); // green
+          this.stateColor.push('rgba(0, 255, 0, 0.4)');
+          document.getElementById('alertCount').innerText = this.alertCount;
+          document.getElementById('alertRate').innerText = (this.alertCount / this.detectedCount).toFixed(2);
+          break;
+        default:
+          this.state.push(0);
+          this.stateColor.push('rgba(0, 255, 0, 0.4)');
+          this.stillDetected = false;
+          this.stillAlert = false;
+          break;
+      }
 
       if (this.timeData.length > this.maxLen) {
         this.timeData.shift();
-        this.temperatureData.shift();
-        this.humidityData.shift();
+        this.accData.shift();
+        this.gyroData.shift();
+        this.state.shift();
+        this.stateColor.shift();
       }
     }
   }
 
-  // All the devices in the list (those that have been sending telemetry)
-  class TrackedDevices {
-    constructor() {
-      this.devices = [];
-    }
-
-    // Find a device based on its Id
-    findDevice(deviceId) {
-      for (let i = 0; i < this.devices.length; ++i) {
-        if (this.devices[i].deviceId === deviceId) {
-          return this.devices[i];
-        }
-      }
-
-      return undefined;
-    }
-
-    getDevicesCount() {
-      return this.devices.length;
-    }
-  }
-
-  const trackedDevices = new TrackedDevices();
+  const monitoredDevice = new DeviceData();
 
   // Define the chart axes
   const chartData = {
     datasets: [
       {
         fill: false,
-        label: 'Temperature',
-        yAxisID: 'Temperature',
+        label: 'Acceleration',
+        yAxisID: 'Acceleration',
         borderColor: 'rgba(255, 204, 0, 1)',
         pointBoarderColor: 'rgba(255, 204, 0, 1)',
         backgroundColor: 'rgba(255, 204, 0, 0.4)',
         pointHoverBackgroundColor: 'rgba(255, 204, 0, 1)',
         pointHoverBorderColor: 'rgba(255, 204, 0, 1)',
         spanGaps: true,
+        type: 'line',
+        order: 0,
       },
       {
         fill: false,
-        label: 'Humidity',
-        yAxisID: 'Humidity',
+        label: 'Gyroscopic',
+        yAxisID: 'Gyroscopic',
         borderColor: 'rgba(24, 120, 240, 1)',
         pointBoarderColor: 'rgba(24, 120, 240, 1)',
         backgroundColor: 'rgba(24, 120, 240, 0.4)',
         pointHoverBackgroundColor: 'rgba(24, 120, 240, 1)',
         pointHoverBorderColor: 'rgba(24, 120, 240, 1)',
         spanGaps: true,
+        type: 'line',
+        order: 1,
+      },
+      {
+        fill: true,
+        label: 'State',
+        yAxisID: 'Acceleration',
+        backgroundColor: 'rgba(0, 255, 0, 0.4)',
+        borderColor: 'rgba(0, 255, 0, 1)',
+        hoverBackgroundColor: 'rgba(0, 255, 0, 1)',
+        hoverBorderColor: 'rgba(0, 100, 0, 1)',
+        type: 'bar',
+        order: 2
       }
     ]
   };
 
+  chartData.datasets[0].data = monitoredDevice.accData;
+  chartData.datasets[1].data = monitoredDevice.gyroData;
+  chartData.datasets[2].data = monitoredDevice.state;
+  chartData.datasets[2].backgroundColor = monitoredDevice.stateColor;
+
   const chartOptions = {
     scales: {
       yAxes: [{
-        id: 'Temperature',
+        id: 'Acceleration',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Temperature (ºC)',
+          labelString: 'Acceleration Vector',
           display: true,
         },
         position: 'left',
         ticks: {
           suggestedMin: 0,
-          suggestedMax: 100,
+          suggestedMax: 8,
           beginAtZero: true
         }
       },
       {
-        id: 'Humidity',
+        id: 'Gyroscopic',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Humidity (%)',
+          labelString: 'Gyroscopic Vector',
           display: true,
         },
         position: 'right',
         ticks: {
           suggestedMin: 0,
-          suggestedMax: 100,
+          suggestedMax: 800,
           beginAtZero: true
         }
-      }]
+      },
+            {
+        id: 'State',
+        type: 'bar',
+        scaleLabel: {
+          labelString: 'State',
+          display: true,
+        },
+        position: 'left', // unsure if this is necessary
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 8,
+          beginAtZero: true
+        }
+      },
+    ]
     }
   };
 
@@ -119,67 +174,25 @@ $(document).ready(() => {
   const myLineChart = new Chart(
     ctx,
     {
-      type: 'line',
+      type: 'bar',
       data: chartData,
       options: chartOptions,
     });
 
-  // Manage a list of devices in the UI, and update which device data the chart is showing
-  // based on selection
-  let needsAutoSelect = true;
-  const deviceCount = document.getElementById('deviceCount');
-  const listOfDevices = document.getElementById('listOfDevices');
-  function OnSelectionChange() {
-    const device = trackedDevices.findDevice(listOfDevices[listOfDevices.selectedIndex].text);
-    chartData.labels = device.timeData;
-    chartData.datasets[0].data = device.temperatureData;
-    chartData.datasets[1].data = device.humidityData;
-    myLineChart.update();
-  }
-  listOfDevices.addEventListener('change', OnSelectionChange, false);
+  // Connect chart to monitored device data
+  chartData.labels = monitoredDevice.timeData;
 
-  // When a web socket message arrives:
-  // 1. Unpack it
-  // 2. Validate it has date/time and temperature
-  // 3. Find or create a cached device to hold the telemetry data
-  // 4. Append the telemetry data
-  // 5. Update the chart UI
+ // Unpack message, validate payload, append to monitored device, update chart
   webSocket.onmessage = function onMessage(message) {
     try {
       const messageData = JSON.parse(message.data);
       console.log(messageData);
 
-      // time and either temperature or humidity are required
-      if (!messageData.MessageDate || (!messageData.IotData.temperature && !messageData.IotData.humidity)) {
+      if (!messageData.MessageDate || (!messageData.IotData.Acceleration && !messageData.IotData.Gyroscopic && !messageData.IotData.FallState)) {
         return;
       }
 
-      // find or add device to list of tracked devices
-      const existingDeviceData = trackedDevices.findDevice(messageData.DeviceId);
-
-      if (existingDeviceData) {
-        existingDeviceData.addData(messageData.MessageDate, messageData.IotData.temperature, messageData.IotData.humidity);
-      } else {
-        const newDeviceData = new DeviceData(messageData.DeviceId);
-        trackedDevices.devices.push(newDeviceData);
-        const numDevices = trackedDevices.getDevicesCount();
-        deviceCount.innerText = numDevices === 1 ? `${numDevices} device` : `${numDevices} devices`;
-        newDeviceData.addData(messageData.MessageDate, messageData.IotData.temperature, messageData.IotData.humidity);
-
-        // add device to the UI list
-        const node = document.createElement('option');
-        const nodeText = document.createTextNode(messageData.DeviceId);
-        node.appendChild(nodeText);
-        listOfDevices.appendChild(node);
-
-        // if this is the first device being discovered, auto-select it
-        if (needsAutoSelect) {
-          needsAutoSelect = false;
-          listOfDevices.selectedIndex = 0;
-          OnSelectionChange();
-        }
-      }
-
+      monitoredDevice.addData(messageData.MessageDate, messageData.IotData.Acceleration, messageData.IotData.Gyroscopic, messageData.IotData.FallState);
       myLineChart.update();
     } catch (err) {
       console.error(err);
